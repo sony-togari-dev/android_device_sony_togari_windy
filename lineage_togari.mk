7471772190cab248c486ca815f604730d1e30d4c
TARKZiM <tom8476oo@gmail.com>
2021-10-03 20:51:50 +0800

    togari: Add back product name overrides

--- lineage_togari.mk
+++ lineage_togari.mk
@@ -3,6 +3,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6833-user 5.0.2 14.5.A.0.270 3305956307 release-keys"
 
 BUILD_FINGERPRINT := Sony/C6833/C6833:5.0.2/14.5.A.0.270/3305956307:user/release-keys
