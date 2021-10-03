dca52a2f5c20ee788fb5a73afc6b6b6bd0f86055
TARKZiM <tom8476oo@gmail.com>
2021-10-03 21:11:03 +0800

    togari: Update build-fingerprint to stock L-MR1

--- lineage_togari.mk
+++ lineage_togari.mk
@@ -4,9 +4,9 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/sony/togari/full_togari.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C6833
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6833-user 5.0.2 14.5.A.0.270 3305956307 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="C6833-user 5.1.1 14.6.A.1.236 834130922 release-keys"
 
-BUILD_FINGERPRINT := Sony/C6833/C6833:5.0.2/14.5.A.0.270/3305956307:user/release-keys
+BUILD_FINGERPRINT := Sony/C6833/C6833:5.1.1/14.6.A.1.236/834130922:user/release-keys
 
 PRODUCT_NAME := lineage_togari
 PRODUCT_DEVICE := togari
